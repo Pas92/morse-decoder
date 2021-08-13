a5c411c7cb77863382c19884cb5ab17da8ca8da5
Pas92 <pavelburdovitsin92@gmail.com>
2021-08-13 17:02:42 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let count = 0;
+    let arrayStr = []; 
+    while(expr.length >= count) {
+        let charBinary = expr.slice(count, count + 10);
+        arrayStr.push(getChar(charBinary));
+        count += 10;
+    }
+
+    
+   
+    function getChar(binary){
+        let charMorze = [];
+
+        if (binary === '**********') {
+            return ' ';
+        }
+
+        for(let i = 0; i<5; i++) {
+            let code = binary.slice(0 + 2*i, 2 + 2*i);
+            switch(code) {
+                case '10': {charMorze.push('.'); break;}
+                case '11': {charMorze.push('-'); break;}
+                default: charMorze.push('');
+            }
+        }
+        let morzeCode = charMorze.join('');
+
+        return MORSE_TABLE[morzeCode];
+    }
+
+    return arrayStr.join('')
 }
 
 module.exports = {
